a8334c4c7f4387dad69b3c01d570d16ff979590e
Leslie-Lu <luzh29@mail2.sysu.edu.cn>
2025-04-21 00:29:00 +0800

    refactor(src): :recycle: Refactor code structure for improved readability and maintainability

--- src/wgs_gwas_and_mr/p007_QC_european.sh
+++ src/wgs_gwas_and_mr/p007_QC_european.sh
@@ -6,6 +6,7 @@
 # INSPIRED BY       : https://documentation.dnanexus.com/user/helpstrings-of-sdk-command-line-utilities#run
 #                   : https://documentation.dnanexus.com/user/running-apps-and-workflows/running-apps-and-applets
 #                   : https://platform.dnanexus.com/app/swiss-army-knife
+#                   : https://cloufield.github.io/GWASTutorial/04_Data_QC/#plink-syntax
 # AUTHOR            : Zhen Lu
 ################################################################################################
 # DATE MODIFIED     : 2025-04-18
@@ -26,3 +27,40 @@
 #   --keep /share/home/lsy_luzhen/WGS_GWAS_and_MR/output/D007EligibleCasesandControlsID_european.txt \
 #   --out /share/home/lsy_luzhen/WGS_GWAS_and_MR/tmp_ssh_data/snp_microarray_european \
 #   --threads $threads
+
+# 2. QC for European ancestry individuals on local
+threads=$(nproc)
+genotypeFile="/share/home/lsy_luzhen/WGS_GWAS_and_MR/tmp_ssh_data/snp_microarray_european"
+output_path="/share/home/lsy_luzhen/WGS_GWAS_and_MR/tmp_ssh_data/QC_european"
+# # Missing rate (call rate)
+# plink \
+#   --bfile ${genotypeFile} \
+#   --missing \
+#   --threads $threads \
+#   --out "$output_path/plink_results_missing_rate"
+# # run WGS_GWAS_and_MR/src/wgs_gwas_and_mr/p008_genotype_data_qc.ipynb
+# # Allele frequency
+# plink \
+#   --bfile ${genotypeFile} \
+#   --freq \
+#   --out "$output_path/plink_result_allele_freq" \
+#   --threads $threads
+# # run WGS_GWAS_and_MR/src/wgs_gwas_and_mr/p008_genotype_data_qc.ipynb
+# # Hardy-Weinberg equilibrium exact test
+# plink \
+#   --bfile ${genotypeFile} \
+#   --hardy \
+#   --out "$output_path/plink_result_hwe" \
+#   --threads $threads
+# # run WGS_GWAS_and_MR/src/wgs_gwas_and_mr/p008_genotype_data_qc.ipynb
+
+# LD pruning
+plink \
+  --bfile ${genotypeFile} \
+  --maf 0.001 \
+  --geno 0.05 \
+  --mind 0.05 \
+  --hwe 1e-6 \
+  --indep-pairwise 50 5 0.2 \
+  --out "$output_path/plink_result_ld_pruning" \
+  --threads $threads
