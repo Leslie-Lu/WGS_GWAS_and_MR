b8f88b442e1e468c20a3a81edccd869a000d0a5b
Leslie-Lu <luzh29@mail2.sysu.edu.cn>
2025-04-21 22:59:51 +0800

    refactor(src): :recycle: Refactor code structure for improved readability and maintainability
    Add output logs for QC and PCA processing of European dataset  - Created output log for QC process on European dataset (p007_QC_european_20250421_131327.out). - Generated output log for LD pruning step (p007_QC_european_20250421_132929.out). - Added output log for kinship cutoff analysis (p007_QC_european_20250421_134811.out). - Documented pre-PCA data preparation log (p007_QC_european_20250421_221446.out).

--- src/wgs_gwas_and_mr/p007_QC_european.sh
+++ src/wgs_gwas_and_mr/p007_QC_european.sh
@@ -28,10 +28,11 @@
 #   --out /share/home/lsy_luzhen/WGS_GWAS_and_MR/tmp_ssh_data/snp_microarray_european \
 #   --threads $threads
 
-# 2. QC for European ancestry individuals on local
-threads=$(nproc)
-genotypeFile="/share/home/lsy_luzhen/WGS_GWAS_and_MR/tmp_ssh_data/snp_microarray_european"
-output_path="/share/home/lsy_luzhen/WGS_GWAS_and_MR/tmp_ssh_data/QC_european"
+# # 2. QC for European ancestry individuals on local
+# threads=$(nproc)
+# genotypeFile="/share/home/lsy_luzhen/WGS_GWAS_and_MR/tmp_ssh_data/snp_microarray_european"
+# output_path="/share/home/lsy_luzhen/WGS_GWAS_and_MR/tmp_ssh_data/QC_european"
+
 # # Missing rate (call rate)
 # plink \
 #   --bfile ${genotypeFile} \
@@ -55,12 +56,78 @@ output_path="/share/home/lsy_luzhen/WGS_GWAS_and_MR/tmp_ssh_data/QC_european"
 # # run WGS_GWAS_and_MR/src/wgs_gwas_and_mr/p008_genotype_data_qc.ipynb
 
 # LD pruning
-plink \
-  --bfile ${genotypeFile} \
-  --maf 0.001 \
-  --geno 0.05 \
-  --mind 0.05 \
-  --hwe 1e-6 \
-  --indep-pairwise 50 5 0.2 \
-  --out "$output_path/plink_result_ld_pruning" \
-  --threads $threads
+# plink \
+#   --bfile ${genotypeFile} \
+#   --maf 0.001 \
+#   --geno 0.05 \
+#   --mind 0.05 \
+#   --hwe 1e-6 \
+#   --indep-pairwise 50 5 0.2 \
+#   --out "$output_path/plink_result_ld_pruning" \
+#   --threads $threads
+
+# Inbreeding F coefficient
+# plink \
+#   --bfile ${genotypeFile} \
+#   --extract "$output_path/plink_result_ld_pruning.prune.in" \
+#   --het \
+#   --out "$output_path/plink_result_inbreeding" \
+#   --threads $threads
+# run WGS_GWAS_and_MR/src/wgs_gwas_and_mr/p008_genotype_data_qc.ipynb
+# awk 'NR>1 && $6>0.0201 || $6<-0.0232 {print $1,$2}' \
+#   "$output_path/plink_result_inbreeding.het" > "$output_path/high_het.sample"
+
+# # Apply all the filters to obtain a clean dataset
+# plink \
+#   --bfile ${genotypeFile} \
+#   --maf 0.001 \
+#   --geno 0.05 \
+#   --mind 0.05 \
+#   --hwe 1e-6 \
+#   --indep-pairwise 50 5 0.2 \
+#   --remove "$output_path/high_het.sample" \
+#   --keep-allele-order \
+#   --make-bed \
+#   --out "$output_path/european_data_pass_qc" \
+#   --threads $threads
+
+# # 3. pre-PCA
+# threads=$(nproc)
+# genotypeFile="/share/home/lsy_luzhen/WGS_GWAS_and_MR/tmp_ssh_data/QC_european/european_data_pass_qc"
+# output_path="/share/home/lsy_luzhen/WGS_GWAS_and_MR/tmp_ssh_data/PCA_european"
+
+# # Create a list of SNPs in high-LD or HLA regions
+# plink \
+#   --bfile ${genotypeFile} \
+#   --make-set /share/home/lsy_luzhen/WGS_GWAS_and_MR/output/high-ld-hg19.txt \
+#   --write-set \
+#   --out "$output_path/hild" \
+#   --threads $threads
+
+# # LD-pruning, excluding high-LD and HLA regions
+# plink2 \
+#   --bfile ${genotypeFile} \
+#   --maf 0.001 \
+#   --exclude "$output_path/hild.set" \
+#   --indep-pairwise 500 50 0.2 \
+#   --out "$output_path/european_ld_pruning" \
+#   --threads $threads
+
+# # remove related samples using king-cutoff
+# plink2 \
+#   --bfile ${genotypeFile} \
+#   --extract "$output_path/european_ld_pruning.prune.in" \
+#   --king-cutoff 0.0884 \
+#   --out "$output_path/european_king_cutoff" \
+#   --threads $threads
+
+# # obtain a clean pre-PCA dataset
+# plink \
+#   --bfile ${genotypeFile} \
+#   --keep "$output_path/european_king_cutoff.king.cutoff.in.id" \
+#   --extract "$output_path/european_ld_pruning.prune.in" \
+#   --keep-allele-order \
+#   --make-bed \
+#   --out "$output_path/european_pre_pca_data" \
+#   --threads $threads
+# # 260007 variants and 195468 people pass filters and QC.
